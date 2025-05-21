6a7bba2567dfd61169ad162503b548f32ba1dfd4
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2025-05-21 11:38:05 -0400

    updates
    updates

--- R/download_module.R
+++ R/download_module.R
@@ -46,51 +46,204 @@ get_gaez_restwo <- function(var,
 
 }
 
+#' Download Koeppen-Geigger (2-character) classification data
+#' 
+#' This function will download KP2 classification data from the google cloud storage drive
+#' 
+#' @param fcp the focal period options "FP2140" for focal period 2021-2040, "FP4160" for
+#' 2041-2060 and likewise for "FP6180" and "FP8100
+#' @param climate_model the climate model used options are IPSL-CM6A-LR, GFDL-ESM4 etc
+#' @param scenario the scenario used for instance SSP126 is the shared socioeconomic pathway
+#' 
+#' 
 
 
-
-
-
-#' A function to create dictionary of file extensions and the functions that
-#' read them so that a do.call can be applied to read in the results
-#'
-
-download_dictionary <- function(){
+get_gaez_kg2 <- function(fcp,
+                         climate_model,
+                         scenario){
   
-  dict_dt <- data.table(file_ext = c("tiff", "tif", "gz"),
-                        opener_function = c("raster", "raster", "gunzip_reader"))
   
-  return(dict_dt)
+  
+}
+
+#' Return the set of files in each GCS repository
+#' 
+#' A function to take in a GCS subdirector url and return the set of files in it. 
+#' 
+#' @param url the link
+#' 
+#' @export
+
+list_gcs_files <- function(folder_url) {
+  # Extract bucket and prefix from the GCS URL
+  if (!grepl("storage.cloud.google.com", folder_url)) {
+    stop("URL must be a Google Cloud Storage folder link")
+  }
+  
+  # Parse bucket and prefix
+  bucket <- sub("https://storage.cloud.google.com/([^/]+).*", "\\1", folder_url)
+  prefix <- sub(sprintf("https://storage.cloud.google.com/%s/?", bucket), "", folder_url)
+  prefix <- gsub("^/", "", prefix)  # remove leading slash
+  if (!endsWith(prefix, "/")) prefix <- paste0(prefix, "/")
+  
+  # Build GCS JSON API endpoint
+  api_url <- sprintf("https://storage.googleapis.com/storage/v1/b/%s/o?prefix=%s", 
+                     bucket, 
+                     URLencode(prefix, 
+                               reserved = TRUE))
+  
+  # Download JSON
+  res <- jsonlite::fromJSON(api_url)
+  
+  # Extract file names
+  if (!"items" %in% names(res)) {
+    return(character(0))  # No items found
+  }
+  
+  files <- res$items$name
+  # Filter just the files in the current folder (no subfolders)
+  files[grepl(sprintf("^%s[^/]+$", prefix), files)]
 }
 
 
-#' A function to download and read in a file from the internet
-#'
-#' @param url a link URL
-#' @param ... additional arguments to be used in `download.file()`
-#'
 
-download_reader <- function(dsn,
-                            url,
-                            ...) {
+download_gcsdir_tifs <- function(folder,
+                                 dsn = "data-raw/gaez"){
   
-  ext <- paste0(".", tools::file_ext(url))
+  base_url <- "https://storage.cloud.google.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET/"
   
-  # temp_file <- tempfile(fileext = ext)
+  url_link <- paste0(base_url, folder, "/")
   
-  utils::download.file(url = url,
-                       destfile = paste(dsn, basename(url), sep = "/"),
-                       mode = "wb",
-                       ...)
+  url_list <- list_gcs_files(url_link)
   
-  dict_dt <- download_dictionary()
+  url_list <- url_list[grepl(".tif", url_list)]
   
-  opener_chr <- dict_dt[file_ext == tools::file_ext(url), opener_function]
   
-  do.call(opener_chr, paste(dsn, basename(url), sep = "/")) ->  raster_obj
+  lapply(X = url_list,
+         FUN = function(x){
+           
+           x <- paste0("https://storage.googleapis.com/fao-gismgr-gaez-v5-data/",
+                       x)
+           
+           download.file(destfile = paste(dsn, 
+                                          basename(x), 
+                                          sep = "/"),
+                         url = x,
+                         mode = "wb")
+           
+           
+         })
   
-  # raster_obj <- crop(raster_obj, extent(shp_dt))
-  
-  return(raster_obj)
   
 }
+
+
+
+#' A function for downloading GAEZ RES05
+#' 
+#' This function will be used to download GAEZ from the google cloud storage drive. 
+#' 
+#' @param var the name of variables of interest within a specific theme
+#' @param crop_code the 4 letter crop name
+#' @param irrigated a logical, if TRUE the irrigated raster will be download otherwise, it is rainfed
+#' @param dsn the folder where the data will be stored
+#' 
+#' 
+
+
+get_gaez_resfive <- function(var,
+                             crop_code,
+                             dsn,
+                             irrigated = T){
+  
+  
+  base_link <- "https://storage.googleapis.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET"
+  
+  theme_var <- paste0("RES05", "-", var)
+  
+  if (irrigated == TRUE){
+    
+    irr <- "HILM"
+    
+  } else {
+    
+    irr <- "HRLM"
+    
+  }
+  
+  full_link <- paste(base_link,
+                     theme_var,
+                     paste0("GAEZ-V5", 
+                            "." , 
+                            theme_var,
+                            ".FP2140.ENSEMBLE.SSP126.", 
+                            crop_code, 
+                            ".", 
+                            irr,
+                            ".tif"),
+                     sep = "/")
+  
+  download.file(destfile = paste(dsn, basename(full_link), sep = "/"),
+                url = full_link,
+                mode = "wb")
+  
+  
+  
+}
+
+
+
+
+
+
+#' A function for downloading GAEZ RES05
+#' 
+#' This function will be used to download GAEZ from the google cloud storage drive. 
+#' 
+#' @param var the name of variables of interest within a specific theme
+#' @param crop_code the 4 letter crop name
+#' @param irrigated a logical, if TRUE the irrigated raster will be download otherwise, it is rainfed
+#' @param dsn the folder where the data will be stored
+#' @param type a character, either "WSI", "WSR" or "WST"
+#' 
+#' 
+
+
+get_gaez_ressix <- function(var,
+                            crop_code,
+                            type,
+                            dsn){
+  
+  
+  base_link <- "https://storage.googleapis.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET"
+  
+  theme_var <- paste0("RES06", "-", var)
+  
+
+  full_link <- paste(base_link,
+                     theme_var,
+                     paste0("GAEZ-V5", 
+                            "." , 
+                            theme_var,
+                            ".",
+                            crop_code, 
+                            ".", 
+                            type,
+                            ".tif"),
+                     sep = "/")
+  
+  download.file(destfile = paste(dsn, basename(full_link), sep = "/"),
+                url = full_link,
+                mode = "wb")
+  
+  
+  
+}
+
+
+
+
+
+
+
+
--- analysis/00-downloadsaverasters.R
+++ analysis/00-downloadsaverasters.R
@@ -3,7 +3,7 @@
 ################################################################################
 
 #### starting with potential yield 
-pak_list <- c("raster", "sf", "tidyverse", "data.table")
+pak_list <- c("raster", "sf", "tidyverse", "data.table", "jsonlite")
 
 lapply(pak_list, function(pkg) {
   suppressMessages(suppressWarnings(
@@ -33,5 +33,249 @@ lapply(X = crop_list,
        var = "YLD",
        irrigated = FALSE,
        dsn = "data-raw/gaez")
+
+
+#### return the set of files in the KG2 folder
+kg2url_list <- list_gcs_files("https://storage.cloud.google.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET/RES01-KG2/")
+
+## keep only the tifs.
+kg2url_list <- kg2url_list[grepl(".tif", kg2url_list)]
+
+## download only focal period 2021 to 2040 projections
+kg2url_list <- kg2url_list[grepl("FP2140", kg2url_list)]
+
+
+lapply(X = kg2url_list,
+       FUN = function(x){
+         
+         x <- paste0("https://storage.googleapis.com/fao-gismgr-gaez-v5-data/",
+                     x)
+         
+         download.file(destfile = paste("data-raw/gaez", 
+                                        basename(x), 
+                                        sep = "/"),
+                       url = x,
+                       mode = "wb")
+         
+         
+       })
+
+### likewise for hte PFR data
+pfrurl_list <- list_gcs_files("https://storage.cloud.google.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET/RES01-PFR/")
+pfrurl_list <- pfrurl_list[grepl(".tif", pfrurl_list)]
+
+
+lapply(X = pfrurl_list,
+       FUN = function(x){
+         
+         x <- paste0("https://storage.googleapis.com/fao-gismgr-gaez-v5-data/",
+                     x)
+         
+         download.file(destfile = paste("data-raw/gaez", 
+                                        basename(x), 
+                                        sep = "/"),
+                       url = x,
+                       mode = "wb")
+         
+         
+       })
+
+#### download all NDD, WDE files with .tif extension
+
+download_gcsdir_tifs(folder = "RES01-NDD")
+
+download_gcsdir_tifs(folder = "RES01-WDE")
+
+
+file_list <- c("LGD", "NDR", "RFM", "RI2", "RQ1", 
+               "RQ2", "RQ3", "RQ4", "LD1")
+
+lapply(X = paste0("RES01-", file_list),
+       FUN = download_gcsdir_tifs)
+
+
+#### download some more RES02 data
+
+ind_list <- c("YLD", "WDE", "ETA", "TSC", "FC2")
+
+grid_dt <- 
+  expand.grid(crop = crop_list, 
+              ind = ind_list) %>%
+  apply(MARGIN = 2, FUN = as.character) %>%
+  as.data.frame() %>%
+  mutate(row_id = row_number()) %>%
+  group_split(row_id, .keep = FALSE)
+
+
+
+###### first download the irrigated
+lapply(X = crop_list,
+       FUN = get_gaez_restwo,
+       var = "YLD",
+       dsn = "data-raw/gaez")
+
+##### then the non-irrigated
+lapply(X = crop_list,
+       FUN = get_gaez_restwo,
+       var = "YLD",
+       irrigated = FALSE,
+       dsn = "data-raw/gaez")
+
+
+
+lapply(X = grid_dt,
+       FUN = function(x){
+         
+         message(sprintf("Downloading indicator '%s' for crop '%s'...", 
+                         x[["ind"]], 
+                         x[["crop"]]))
+         
+         tryCatch({
+           
+           get_gaez_restwo(crop_code = x[["crop"]], 
+                           var = x[["ind"]], 
+                           dsn = "data-raw/gaez")
+           
+         }, error = function(e){
+           
+           message(sprintf("failed to download for indicator '%s' for crop '%s' : %s", 
+                           x[["ind"]], 
+                           x[["crop"]],
+                           e$message))
+           
+           return(NULL)
+           
+         })
+         
+         
+         
+       })
+
+
+ind_list <- c("SCX", "SIX", "SUX", "SXX")
+
+crop_list <- c("BRL", "MZE", "RCW", "SRG", "WHE")
+
+grid_dt <- 
+  expand.grid(crop = crop_list,
+              ind = ind_list) %>%
+  apply(MARGIN = 2, FUN = as.character) %>%
+  as.data.frame() %>%
+  mutate(row_id = row_number()) %>%
+  group_split(row_id, .keep = FALSE)
+
+
+
+lapply(X = grid_dt,
+       FUN = function(x){
+         
+         message(sprintf("Downloading indicator '%s' for crop '%s'...", 
+                         x[["ind"]], 
+                         x[["crop"]]))
+         
+         ### for the irrigated cropland
+         tryCatch({
+           
+           get_gaez_resfive(crop_code = x[["crop"]], 
+                            var = x[["ind"]], 
+                            dsn = "data-raw/gaez")
+           
+         }, error = function(e){
+           
+           message(sprintf("failed to download for indicator '%s' for crop '%s' : %s", 
+                           x[["ind"]], 
+                           x[["crop"]],
+                           e$message))
+           
+           return(NULL)
+           
+         })
+         
+         ### for rainfed cropland
+         tryCatch({
+           
+           get_gaez_resfive(crop_code = x[["crop"]], 
+                            var = x[["ind"]], 
+                            dsn = "data-raw/gaez",
+                            irrigated = FALSE)
+           
+         }, error = function(e){
+           
+           message(sprintf("failed to download for indicator '%s' for crop '%s' : %s", 
+                           x[["ind"]], 
+                           x[["crop"]],
+                           e$message))
+           
+           return(NULL)
+           
+         })
+         
+         
+       })
+
+### download the res06 data
+ind_list <- c("HAR", "YLD", "PRD")
+
+crop_list <- c("BRL", "MZE", "RCW", "SRG", "WHE", "MLT")
+
+type_list <- c("WSI", "WSR", "WST")
+
+grid_dt <- 
+  expand.grid(crop = crop_list,
+              ind = ind_list,
+              type = type_list) %>%
+  apply(MARGIN = 2, FUN = as.character) %>%
+  as.data.frame() %>%
+  mutate(row_id = row_number()) %>%
+  group_split(row_id, .keep = FALSE)
+
+  
+
+lapply(X = grid_dt,
+       FUN = function(x){
+         
+         tryCatch({
+           
+           get_gaez_ressix(crop_code = x[["crop"]], 
+                           var = x[["ind"]], 
+                           type = x[["type"]],
+                           dsn = "data-raw/gaez")
+           
+         }, error = function(e){
+           
+           message(sprintf("failed to download for indicator '%s' for crop '%s' of type '%s' : %s", 
+                           x[["ind"]], 
+                           x[["crop"]],
+                           x[["type"]],
+                           e$message))
+           
+           return(NULL)
+           
+         })
+         
+         
+         
+       })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
