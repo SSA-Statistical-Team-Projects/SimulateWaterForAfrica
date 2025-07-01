bedecce1483db032c1dcb2672136fff44b970b7f
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2025-07-01 10:02:32 -0400

    updates
    updates

--- R/download_module.R
+++ R/download_module.R
@@ -6,12 +6,15 @@
 #' @param crop_code the 4 letter crop name
 #' @param irrigated a logical, if TRUE the irrigated raster will be download otherwise, it is rainfed
 #' @param dsn the folder where the data will be stored
+#' @param time_period the time period (e.g., HP8100 for 1981-2000, HP0120 for 2001-2020, FP2140 for 2021-2040, 
+#' FP4160 for 2041-2060, FP6180 for 2061-2080 and FP8100 for 2081-2100) or the year for time-series data
 
 
 get_gaez_restwo <- function(var,
                             crop_code,
                             irrigated = T,
-                            dsn){
+                            dsn,
+                            time_period = "FP2140"){
   
   base_link <- "https://storage.googleapis.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET"
   
@@ -32,7 +35,9 @@ get_gaez_restwo <- function(var,
                      paste0("GAEZ-V5", 
                             "." , 
                             theme_var,
-                            ".FP2140.GFDL-ESM4.SSP126.", 
+                            ".",
+                            time_period,
+                            ".GFDL-ESM4.SSP126.", 
                             crop_code, 
                             ".", 
                             irr,
@@ -44,6 +49,55 @@ get_gaez_restwo <- function(var,
                 mode = "wb")
   
 
+}
+
+
+get_gaez_restwo_v2 <- function(var,
+                               crop_code,
+                               water_supply = "H",
+                               irrigated = T,
+                               dsn,
+                               time_period = "FP2140"){
+  
+  base_link <- "https://storage.googleapis.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET"
+  
+  theme_var <- paste0("RES02", "-", var)
+  
+  irr_dt <- 
+    tibble(ws = water_supply,
+           irrigated = ifelse(isTRUE(irrigated), "I", "R"),
+           ext = "LM") |>
+    mutate(water_supply = paste0(ws, irrigated, ext))
+
+  
+
+  if (grepl(pattern = "HP", x = time_period)) {
+    
+    time_period <- paste0(time_period, ".AGERA5.HIST.")
+    
+  } else {
+    
+    time_period <- paste0(time_period, ".GFDL-ESM4.SSP126.")
+  }
+  
+  full_link <- paste(base_link,
+                     theme_var,
+                     paste0("GAEZ-V5", 
+                            "." , 
+                            theme_var,
+                            ".",
+                            time_period,
+                            crop_code, 
+                            ".", 
+                            irr_dt$water_supply,
+                            ".tif"),
+                     sep = "/")
+  
+  download.file(destfile = paste(dsn, basename(full_link), sep = "/"),
+                url = full_link,
+                mode = "wb")
+  
+  
 }
 
 #' Download Koeppen-Geigger (2-character) classification data
@@ -70,41 +124,36 @@ get_gaez_kg2 <- function(fcp,
 #' 
 #' A function to take in a GCS subdirector url and return the set of files in it. 
 #' 
-#' @param url the link
+#' @param folder_url the link
 #' 
 #' @export
 
 list_gcs_files <- function(folder_url) {
-  # Extract bucket and prefix from the GCS URL
   if (!grepl("storage.cloud.google.com", folder_url)) {
     stop("URL must be a Google Cloud Storage folder link")
   }
-  
-  # Parse bucket and prefix
   bucket <- sub("https://storage.cloud.google.com/([^/]+).*", "\\1", folder_url)
   prefix <- sub(sprintf("https://storage.cloud.google.com/%s/?", bucket), "", folder_url)
   prefix <- gsub("^/", "", prefix)  # remove leading slash
   if (!endsWith(prefix, "/")) prefix <- paste0(prefix, "/")
-  
-  # Build GCS JSON API endpoint
-  api_url <- sprintf("https://storage.googleapis.com/storage/v1/b/%s/o?prefix=%s", 
-                     bucket, 
-                     URLencode(prefix, 
-                               reserved = TRUE))
-  
-  # Download JSON
-  res <- jsonlite::fromJSON(api_url)
-  
-  # Extract file names
-  if (!"items" %in% names(res)) {
-    return(character(0))  # No items found
+  all_files <- c()
+  page_token <- NULL
+  repeat {
+    api_url <- sprintf("https://storage.googleapis.com/storage/v1/b/%s/o?prefix=%s",
+                       bucket, URLencode(prefix, reserved = TRUE))
+    if (!is.null(page_token)) {
+      api_url <- paste0(api_url, "&pageToken=", page_token)
+    }
+    res <- jsonlite::fromJSON(api_url)
+    if ("items" %in% names(res)) {
+      all_files <- c(all_files, res$items$name)
+    }
+    if (!"nextPageToken" %in% names(res)) break
+    page_token <- res$nextPageToken
   }
-  
-  files <- res$items$name
-  # Filter just the files in the current folder (no subfolders)
-  files[grepl(sprintf("^%s[^/]+$", prefix), files)]
+  # Filter to files in the exact folder (not subfolders)
+  all_files[grepl(sprintf("^%s[^/]+$", prefix), all_files)]
 }
-
 
 
 download_gcsdir_tifs <- function(folder,
@@ -191,8 +240,55 @@ get_gaez_resfive <- function(var,
   
 }
 
-
-
+get_gaez_resfive_v2 <- function(var,
+                                crop_code,
+                                water_supply,
+                                time_period = "FP2140",
+                                dsn,
+                                irrigated = T){
+  
+  
+  base_link <- "https://storage.googleapis.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET"
+  
+  theme_var <- paste0("RES05", "-", var)
+  
+  irr_dt <- 
+    tibble(ws = water_supply,
+           irrigated = ifelse(isTRUE(irrigated), "I", "R"),
+           ext = "LM") |>
+    mutate(water_supply = paste0(ws, irrigated, ext))
+  
+  
+  
+  if (grepl(pattern = "HP", x = time_period)) {
+    
+    time_period <- paste0(time_period, ".AGERA5.HIST.")
+    
+  } else {
+    
+    time_period <- paste0(time_period, ".ENSEMBLE.SSP126.")
+  }
+  
+  full_link <- paste(base_link,
+                     theme_var,
+                     paste0("GAEZ-V5", 
+                            "." , 
+                            theme_var,
+                            ".",
+                            time_period, 
+                            crop_code, 
+                            ".", 
+                            irr_dt$water_supply,
+                            ".tif"),
+                     sep = "/")
+  
+  download.file(destfile = paste(dsn, basename(full_link), sep = "/"),
+                url = full_link,
+                mode = "wb")
+  
+  
+  
+}
 
 
 
@@ -239,6 +335,75 @@ get_gaez_ressix <- function(var,
   
   
 }
+
+
+get_gaez_resone_ts <- function(theme_var,
+                               year = 2020,
+                               dsn){
+  
+  base_link <- "https://storage.googleapis.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET"
+  
+  theme_var <- paste0("RES01-", theme_var, "-TS")
+  
+  full_link <- paste(base_link,
+                     theme_var,
+                     paste0("GAEZ-V5", 
+                            "." , 
+                            theme_var,
+                            ".",
+                            year,
+                            ".tif"),
+                     sep = "/")
+  
+  download.file(destfile = paste(dsn, basename(full_link), sep = "/"),
+                url = full_link,
+                mode = "wb")
+  
+  
+}
+
+#' Get the landcover datasets
+#' 
+#' @param class, an `integer`, the landcover class number 
+#' @param dsn `character` the file path where the data will be stored
+#' 
+#' 
+get_gaez_lrlcc <- function(class,
+                           dsn){
+  
+  
+  base_link <- "https://storage.googleapis.com/fao-gismgr-gaez-v5-data/DATA/GAEZ-V5/MAPSET"
+  
+  lcc_file <- paste0("/LR-LCC/GAEZ-V5.LR-LCC.LC",
+                     sprintf("%02d", class),
+                     ".tif")
+  
+  full_link <- paste0(base_link, lcc_file)
+  
+  download.file(destfile = paste(dsn, basename(full_link), sep = "/"),
+                url = full_link,
+                mode = "wb")
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
